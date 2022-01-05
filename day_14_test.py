8ca91c066b95fdafb3a8bebae46692ab971e0f78
rnoriega <rnoriega@flameanalytics.com>
2022-01-05 02:50:38 +0100

    Remove print

--- day_14_test.py
+++ day_14_test.py
@@ -8,7 +8,6 @@ class TestDay14(TestCase):
         self._reindeer = list(map(parse_reindeer,
                                   ['Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.',
                                    'Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.']))
-        print(self._reindeer)
 
     def test_part_1(self):
         self.assertEqual(part_1(self._reindeer, 1_000), 1_120)
