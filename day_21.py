378798569a4a7317a2a50f80bfc87087f3e50224
rnoriega <rnoriega@flameanalytics.com>
2022-01-09 19:12:09 +0100

    Day 21: change monster to boss

--- day_21.py
+++ day_21.py
@@ -19,32 +19,32 @@ RINGS = [(0, 0, 0),
 
 
 def main():
-    monster_stats = int(input().split()[-1]), int(input().split()[-1]), int(input().split()[-1])
+    boss_stats = int(input().split()[-1]), int(input().split()[-1]), int(input().split()[-1])
 
-    print(part_1(monster_stats))
-    print(part_2(monster_stats))
+    print(part_1(boss_stats))
+    print(part_2(boss_stats))
 
 
-def part_1(monster_stats):
+def part_1(boss_stats):
     return min(weapon_cost + armor_cost + ring_1_cost + ring_2_cost
                for weapon_cost, weapon_damage in WEAPONS
                for armor_cost, armor_armor in ARMOR
                for ring_1_cost, ring_1_damage, ring_1_armor in RINGS
                for ring_2_cost, ring_2_damage, ring_2_armor in RINGS
                if (ring_1_cost != ring_2_cost or ring_1_cost == 0) and
-               monster_stats[0] / max(weapon_damage + ring_1_damage + ring_2_damage - monster_stats[2], 1)
-               <= 100 / max(monster_stats[1] - armor_armor - ring_1_armor - ring_2_armor, 1))
+               boss_stats[0] / max(weapon_damage + ring_1_damage + ring_2_damage - boss_stats[2], 1)
+               <= 100 / max(boss_stats[1] - armor_armor - ring_1_armor - ring_2_armor, 1))
 
 
-def part_2(monster_stats):
+def part_2(boss_stats):
     return max(weapon_cost + armor_cost + ring_1_cost + ring_2_cost
                for weapon_cost, weapon_damage in WEAPONS
                for armor_cost, armor_armor in ARMOR
                for ring_1_cost, ring_1_damage, ring_1_armor in RINGS
                for ring_2_cost, ring_2_damage, ring_2_armor in RINGS
                if (ring_1_cost != ring_2_cost or ring_1_cost == 0) and
-               monster_stats[0] / max(weapon_damage + ring_1_damage + ring_2_damage - monster_stats[2], 1)
-               > 100 / max(monster_stats[1] - armor_armor - ring_1_armor - ring_2_armor, 1))
+               boss_stats[0] / max(weapon_damage + ring_1_damage + ring_2_damage - boss_stats[2], 1)
+               > 100 / max(boss_stats[1] - armor_armor - ring_1_armor - ring_2_armor, 1))
 
 
 if __name__ == '__main__':
